a6cec19f5bbb86a86c00e5396854c85819e6dd85
Kristen Moore <kristen@codefu.org>
2017-05-26 22:21:25 -0500

    Write basic print lines and mixed in some calculations

---
+++ hello_world.py
@@ -0,0 +1,23 @@
+# LEARN PYTHON THE HARD WAY
+
+# Exercise 1 
+print "Hello World!"
+print "Python print statements are preceded by 'print', versus 'puts' in Ruby"
+print "Hey %s there." % "you"
+
+# Exercise 4
+cars = 100
+space_in_a_car = 4
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+print "There are", cars,"cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "passengers to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
