faecf8078e6d7ac4be4bd39f5bc5504560b20a69
Kristen Moore <kristen@codefu.org>
2017-05-28 23:52:02 -0500

    fix typos

--- hello_world1.py
+++ hello_world1.py
@@ -134,7 +134,5 @@ raw_input()
 out_file = open(to_file, 'w')
 out_file.write(indata)
 
-print "Alright, all done."
-
 out_file.close()
 # in_file.close()
--- roman_numerals.py
+++ roman_numerals.py
@@ -1,2 +1,3 @@
 # This is a method that converts an integer to its Roman numeral equivalent
 
+
